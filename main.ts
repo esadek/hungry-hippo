2d5331db74af5902646ecddf8c09a9884c7ae6fd
Emil Sadek <esadek@hotmail.com>
2023-11-14 19:39:37 -0800

    Add logging

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
+import * as log from "https://deno.land/std@0.206.0/log/mod.ts";
+
 Deno.serve(async (req: Request): Promise<Response> => {
   if (req.method !== "POST") {
+    log.error("405 Method Not Allowed");
     return new Response("Only POST requests accepted", { status: 405 });
   }
   try {
     const payload = await req.json();
     const output = JSON.stringify(payload) + "\n";
     await Deno.writeTextFile("data.jsonl", output, { append: true });
+    log.info(payload);
+    log.info("200 OK");
     return new Response("Payload consumed and stored", { status: 200 });
   } catch (error) {
+    log.error("400 Bad Request");
     return new Response(error, { status: 400 });
   }
 });
